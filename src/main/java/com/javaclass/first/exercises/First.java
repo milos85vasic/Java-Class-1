da6b19b4d50eac024557ee3c0997203ef6ea6e9c
Milos Vasic <milos85vasic@gmail.com>
2017-12-17 20:16:34 +0100

    Working with comments.

--- src/main/java/com/javaclass/first/exercises/First.java
+++ src/main/java/com/javaclass/first/exercises/First.java
@@ -1,7 +1,15 @@
 package com.javaclass.first.exercises;
 
-public class First {
+/**
+ * This is the first class we create.
+ * It contains some examples of Java basic data types.
+ */
+public class First { // This is example of one line comment.
+    /*
+     * Another multiline comment.
+     * Second row.
+     */
 
-
-
+    // Example of code before the comment and the comment after the code:
+    int x = 0; // Another single line comment: int x = 0;
 }
